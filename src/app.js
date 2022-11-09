6c534ebe0ec48b9a79ddefdb2ca03277851f4c41
Facundo Undiano <facundoundiano@gmail.com>
2022-11-08 23:23:23 -0300

    static files

--- src/app.js
+++ src/app.js
@@ -2,20 +2,18 @@
 //Requerir paquetes express y path
 const express = require('express');
 const app = express();
-const mainRouter = require('./routers/mainRouter');
 const path = require('path');
+const mainRouter = require('./routers/mainRouter');
 
 // // ////////////////////////////////////////////
 //Abriendo puerto 3000
-let PORT = process.env.PORT || 3030; //El web hosting le da un puerto a tu proyecto, sino toma el puerto 3030
+let PORT = process.env.PORT || 3000; //El web hosting le da un puerto a tu proyecto, sino toma el puerto 3030
 app.listen(PORT, () => {
   console.log('servidor corriendo en puerto ' + PORT);
 });
 //////////////////////////////////////////////////
-//Hago pública la carpeta public
-const publicPath = path.resolve(__dirname, './public');
 //Para usar carpetas estáticas
-app.use(express.static('public'));
+app.use(express.static(path.resolve(__dirname,'../public')));
 //////////////////////////////////////////////////
 
 app.use('/', mainRouter)
